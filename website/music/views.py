87c8b7fff3aaa3e2768cb2ced5a5efed1d394451
maainul <mainul080@yahoo.com>
2018-09-08 21:30:42 +0600

    new file added

--- website/music/views.py
+++ website/music/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from .models import Album
 from django.http import HttpResponse
+from django.shortcuts import HttpResponse
 
 def index(request):
 	all_albums = Album.objects.all()
-	html=''
-	for album in all_albums:
-		url = '/music/' + str(album.id) + '/'
-		html += '<a href =" ' + url + '">' + album.album_title + '</a></br>'
-	return HttpResponse(html)
+	#we use shortcuts so remove this line and return line also
+	#template = loader.get_template('music/index.html')
+	context ={'all_albums':all_albums}
+	return render(request,'music/index.html',context)
+	#return HttpResponse(template.render(context,request))
 
 def detail(request,album_id):#album_id is like a variable
 	return HttpResponse("<h1>Details for album id:" +str(album_id)+ "</h1>")
